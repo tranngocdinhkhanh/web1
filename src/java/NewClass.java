7062eeb0f49271af8e30af1f3e10345761e05b37
KhanhTran <tranngocdinhkhanh@gmail.com>
2021-06-02 16:16:55 +0700

    add ngu vl

--- src/java/NewClass.java
+++ src/java/NewClass.java
@@ -11,6 +11,6 @@
 public class NewClass {
     public static void main(String[] args) {
         System.out.println("cac");
-        System.out.println("vl);
+        System.out.println("vl");
     }
 }
